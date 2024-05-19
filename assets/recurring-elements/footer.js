24257d5821dc456958c557fb0be705a1cf0aee67
Nirvek <nirvekpandey@gmail.com>
2024-05-19 11:19:55 -0700

    Update footer.js

--- assets/recurring-elements/footer.js
+++ assets/recurring-elements/footer.js
@@ -1,18 +1,19 @@
-const footerHTML = `
-<footer class="footer">
-  <div class="footer-container">
-    <p>11:eleven</p>
-  </div>
-</footer>
-`;
+
 
 function loadFooter() {
-    const footerPlaceholder = document.getElementById('footer');
-    if (footerPlaceholder) {
-        footerPlaceholder.innerHTML = footerHTML;
-    } else {
-        console.error('Could not find insert-footer element');
-    }
+  const footerPlaceholder = document.getElementById('footer');
+  const footerHTML = `
+  <footer class="footer">
+    <div class="footer-container">
+      <p>11:eleven</p>
+    </div>
+  </footer> 
+`;
+  if (footerPlaceholder) {
+    footerPlaceholder.innerHTML = footerHTML;
+  } else {
+    console.error('Could not find footer element');
+  }
 }
 
 document.addEventListener('DOMContentLoaded', loadFooter);
