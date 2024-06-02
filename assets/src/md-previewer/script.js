5c5483774e55c9d1bf47433db52b3e1de2a39677
hhajibadri <hhajibadri@ucsd.edu>
2024-06-02 01:36:02 -0700

    finished the md previewer mostly

--- assets/src/md-previewer/script.js
+++ assets/src/md-previewer/script.js
@@ -1,8 +1,24 @@
 import { marked } from 'https://cdn.jsdelivr.net/npm/marked/lib/marked.esm.js';
 
-const content = document.getElementById('text-content');
+const contents = document.getElementById('text-content');
 const parsedContent = document.getElementById('md-parser');
 
-content.addEventListener('input', () => {
-    parsedContent.innerHTML = marked.parse(content.value);
+marked.use({
+    breaks: true,
+    gfm: true,
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+    const text = localStorage.getItem('content');
+    if(text){
+        parsedContent.innerHTML = marked.parse(text);
+        contents.value = text;
+    }
+})
+
+contents.addEventListener('input', () => {
+    const text = contents.value.replace(/^[\u200B\u200C\u200D\u200E\u200F\uFEFF]/,'');
+    console.log(text);
+    parsedContent.innerHTML = marked.parse(text);
+    localStorage.setItem('content', text);
 });
