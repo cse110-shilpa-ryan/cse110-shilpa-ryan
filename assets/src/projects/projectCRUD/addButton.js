76fb0fd5f6c9c50632dcc5642c98f0db693528f2
Nirvek <nirvekpandey@gmail.com>
2024-06-05 00:51:29 -0700

    moved add button to top right corner, inside nav bar

---
+++ assets/src/projects/projectCRUD/addButton.js
@@ -0,0 +1,22 @@
+/**
+ * Adds a new button element to the navigation bar with the text "Add Project".
+ * @function addButton
+ */
+export function addButton() {
+
+    let navButtonsContainer = document.getElementsByClassName('nav-buttons')[0];
+    if (navButtonsContainer) {
+        // Create the new button element
+        const addButton = document.createElement('div');
+
+        addButton.classList.add('nav-button');
+        addButton.id = 'add-project-button'; // A unique ID for the button
+        addButton.textContent = 'Add Project';
+
+        // Append the button to the navigation bar
+        navButtonsContainer.appendChild(addButton);
+    } else {
+        console.error('Could not find the navigation buttons container');
+    }
+}
+
