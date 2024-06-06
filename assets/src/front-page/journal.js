a3783209b606677794f75e093c936251edd836c6
Evan Wu <ehw014@ucsd.edu>
2024-06-06 13:54:19 -0700

    separated journal container for functionality, improved styling, begun rendering progess bar

--- assets/src/front-page/journal.js
+++ assets/src/front-page/journal.js
@@ -14,28 +14,64 @@ document.addEventListener('DOMContentLoaded', function() {
 
         return journals.filter(journal => {
             let journalDate = new Date(journal.id);
-            console.log(journalDate.toDateString());
+            //console.log(journalDate.toDateString());
             return journalDate >= nextFiveDays;
         }).sort((a,b) => b.id - a.id);
     }
 
-    /*function getJournalDays() {
-        const journals = getJournalEntires();
+    function getJournalDays() {
+        const journals = getJournalEntries();
         const today = new Date();
-        const nextSevenDays = new Date();
+        let nextSevenDays = new Date();
         nextSevenDays.setDate(today.getDate() - 7);
 
         let week = new Array();
         for(let i = 0; i < 7; i++) {
             if ( 0 !== journals.filter(journal => {
                 let journalDate = new Date(journal.id);
-                return journalDate === nextSevenDays;
+                return (journalDate.getDate() === nextSevenDays.getDate()
+                        && journalDate.getMonth() === nextSevenDays.getMonth()
+                        && journalDate.getFullYear() === nextSevenDays.getFullYear());
             }).length)
-                week.push(true);
+                week.push([true, nextSevenDays.getDay()]);
             else
-                week.push(false);
+                week.push([false, nextSevenDays.getDay()]);
+            nextSevenDays.setDate(nextSevenDays.getDate() + 1);
         }
-    }*/
+        console.log(week);
+        return(week);
+    }
+
+    function renderWeeklyBar() {
+        const weeklyProgress = getJournalDays();
+        let progressContainer = document.getElementById('weekly-progress');
+        let progressBar = document.getElementById('progress-bar');
+
+        for(let i = 0; i < weeklyProgress.length; i++) {
+            let dayBar = document.createElement('div');
+            dayBar.id = 'daily-bar';
+            if(weeklyProgress[i][0])
+                dayBar.className = "present";
+            else
+                dayBar.className = "absent";
+            dayBar.textContent = dayAbbreviation(weeklyProgress[i][1]);
+            progressBar.appendChild(dayBar);
+        }
+
+        let statusMessage = document.querySelector("#weekly-progress  h3");
+        statusMessage.innerHTML = 'haha';
+    }
+    function dayAbbreviation(index) {
+        switch(index) {
+            case 0: return "S";
+            case 1: return "M";
+            case 2: return "Tu";
+            case 3: return "W";
+            case 4: return "Th";
+            case 5: return "F";
+            case 6: return "S";
+        }
+    }
 
     // Render recent journals
     function renderRecentJournals() {
@@ -54,7 +90,8 @@ document.addEventListener('DOMContentLoaded', function() {
             });
         }
     }
-
+    renderWeeklyBar();
+    getJournalDays();
     renderRecentJournals();
 });
 
